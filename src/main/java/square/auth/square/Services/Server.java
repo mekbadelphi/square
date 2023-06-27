82be6f7e5345f1d080d065d057ab12f6c2493408
Mehrdad E <112664950+mekbadelphi@users.noreply.github.com>
2023-06-27 16:28:54 -0700

    callback modified

--- src/main/java/square/auth/square/Services/Server.java
+++ src/main/java/square/auth/square/Services/Server.java
@@ -1,20 +1,47 @@
 package square.auth.square.Services;
 
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class Server {
-    @RequestMapping("/callback")
-    public String getAllTopics() {
-        System.out.println("callback was called");
-        return "tr.returnResult().toString()";
-    }
+    Logger logger = LoggerFactory.getLogger(Server.class);
+//    @RequestMapping(method = RequestMethod.GET, value = "/callback")
+//    public String getAllTopics() {
+//        System.out.println("callback was called");
+//        return "tr.returnResult().toString()";
+//    }
+//    @RequestMapping(method = RequestMethod.GET, value = "/callback")
+//    public ResponseEntity<String> getHeader(@RequestHeader("User-Agent") String userAgent) {
+//        // Construct response
+//        HttpHeaders headers = new HttpHeaders();
+//        headers.add("Access-Control-Allow-Origin", "*");
+//        headers.add("Content-Type", "application/json");
+//        headers.add("Content-Type", "application/json");
+//
+//        String response = "Header:\nUser-Agent: " + userAgent;
+//        logger.info(response);
+//        return ResponseEntity
+//                .status(HttpStatus.OK)
+//                .headers(headers)
+//                .body(response);
+//    }
+    @RequestMapping(method = RequestMethod.GET, value = "/callback")
+    public ResponseEntity<String> getCode(@RequestParam("code") String code, @RequestParam("response_type") String responseType){
+        // Construct response
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Access-Control-Allow-Origin", "*");
+        headers.add("Content-Type", "application/json");
 
-    @RequestMapping("/callback")
-    public String getAllTopics2(@RequestBody String payload) {
-        System.out.println("callback was called");
-        return "return with param" + payload;
+        String response = "Code: " + code +"\nResponse Type: " + responseType;
+        logger.info(response);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .headers(headers)
+                .body(response);
     }
 }
--- src/main/java/square/auth/square/SquareApplication.java
+++ src/main/java/square/auth/square/SquareApplication.java
@@ -2,16 +2,16 @@ package square.auth.square;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import static square.auth.square.Quickstart.getAccessToken;
 
 @SpringBootApplication
-public class SquareApplication {
+public class SquareApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
-
         SpringApplication.run(SquareApplication.class, args);
-        Quickstart.getAccessToken();
+//        Quickstart.getAccessToken();
     }
 
 }
